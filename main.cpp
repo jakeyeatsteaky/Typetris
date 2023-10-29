6e69b1080950072d76900593103e5c7ac05d86aa
Jake Baker <jakebaker@pop-os.localdomain>
2023-10-28 17:01:10 -0700

    update todo

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include "App.h"
 #include "enumerations.h"
 
+#if 0
+	TODO:
+
+		[ ] Render a square.  Maybe a function which can take coordinates
+		
+#endif
+
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 {
 	App app;
